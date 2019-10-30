2f72a139eae0ff87c965f27eb6b455d5b070ef3f
Andreas Malmström <maan1686@student.ju.se>
2019-10-30 10:24:46 +0100

    Initial Commit

---
+++ Laboration2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Laboration2
+//
+//  Created by Andreas Malmström on 2019-10-30.
+//  Copyright © 2019 Andreas Malmström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
