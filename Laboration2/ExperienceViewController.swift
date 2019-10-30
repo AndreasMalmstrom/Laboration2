370415cad6fe05f148e9ba5f290a1c7d6e459bdb
Andreas Malmström <maan1686@student.ju.se>
2019-10-30 16:25:49 +0100

    Made a segue I think

---
+++ Laboration2/ExperienceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExperienceViewController.swift
+//  Laboration2
+//
+//  Created by Andreas Malmström on 2019-10-30.
+//  Copyright © 2019 Andreas Malmström. All rights reserved.
+//
+
+import UIKit
+
+class ExperienceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
